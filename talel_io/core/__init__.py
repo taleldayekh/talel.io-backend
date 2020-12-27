7f81430cad5f3d51a1d97ba17f119bb8e576d644
taleldayekh <mail@talel.se>
2020-12-27 14:05:20 +0100

    Enabling CORS
    CORS is temporarily enabled globally as an attempt to access the API via AWS.

--- talel_io/core/__init__.py
+++ talel_io/core/__init__.py
@@ -1,10 +1,14 @@
+# mypy: ignore-errors
 from flask import Flask
+from flask_cors import CORS
 
 from talel_io.interfaces.api.user.user_controller import user_v1
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    CORS(app)
+
     app.register_blueprint(user_v1, url_prefix='/api/v1/user')
 
     return app
