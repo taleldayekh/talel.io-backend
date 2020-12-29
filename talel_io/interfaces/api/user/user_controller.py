8ac8955303f44170d9adac6e37f7c980f6b47c2a
taleldayekh <mail@talel.se>
2020-12-29 13:46:56 +0100

    Returning regular string from API endpoint
    This commit is for debugging the CORS issues when deploying the API to AWS.

--- talel_io/interfaces/api/user/user_controller.py
+++ talel_io/interfaces/api/user/user_controller.py
@@ -1,11 +1,12 @@
 from typing import Any
 
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 user_v1 = Blueprint('user_v1', __name__)
 
 
 @user_v1.route('/', methods=['GET'])
 def user() -> Any:
-    user_response = {'message': 'talel.io API'}
-    return jsonify(user_response), 200
+    return 'Hello World'
+    # user_response = {'message': 'talel.io API'}
+    # return jsonify(user_response), 200
