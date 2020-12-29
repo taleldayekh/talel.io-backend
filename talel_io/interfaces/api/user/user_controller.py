8b1eb1f2016328e14c5bb3e05c72c09b938d210b
taleldayekh <mail@talel.se>
2020-12-29 14:59:26 +0100

    Reverting back to full API endpoint
    Defining a host and a port seems to have solved the connection refused issue on AWS.

--- talel_io/interfaces/api/user/user_controller.py
+++ talel_io/interfaces/api/user/user_controller.py
@@ -1,12 +1,11 @@
 from typing import Any
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 user_v1 = Blueprint('user_v1', __name__)
 
 
 @user_v1.route('/', methods=['GET'])
 def user() -> Any:
-    return 'Hello World'
-    # user_response = {'message': 'talel.io API'}
-    # return jsonify(user_response), 200
+    user_response = {'message': 'talel.io API'}
+    return jsonify(user_response), 200
--- talel_io/tests/e2e/test_user_controller.py
+++ talel_io/tests/e2e/test_user_controller.py
@@ -1,6 +1,6 @@
 # pylint: disable=E1101, R0903
 # mypy: ignore-errors
-# import json
+import json
 
 import pytest
 
@@ -8,11 +8,8 @@ import pytest
 @pytest.mark.usefixtures('api_server')
 class TestUserGET:
     def test_get_user_returns_200(self) -> None:
-        # res = self.api.get('api/v1/user/')
-        res = self.api.get('/')
+        res = self.api.get('api/v1/user/')
+        res_data = json.loads(res.get_data(as_text=True))
 
+        assert res_data['message'] == 'talel.io API'
         assert res.status_code == 200
-        # res_data = json.loads(res.get_data(as_text=True))
-
-        # assert res_data['message'] == 'talel.io API'
-        # assert res.status_code == 200
