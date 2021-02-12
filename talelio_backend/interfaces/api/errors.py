f93f5c09f54e68927ae49dd7596e50a81c16c121
taleldayekh <mail@talel.se>
2021-02-12 09:02:32 +0100

    Unifying API error handling logic

---
+++ talelio_backend/interfaces/api/errors.py
@@ -0,0 +1,28 @@
+from typing import Dict, NewType, Tuple
+
+from flask import Flask, jsonify
+
+ErrorResponse = NewType('ErrorResponse', Dict[str, Dict[str, object]])
+
+
+class APIError(Exception):
+    http_response_status_codes = {400: 'Bad Request'}
+
+    def __init__(self, message: str, status_code: int) -> None:
+        super().__init__()
+        self.status_code = status_code
+        self.status_type = self.http_response_status_codes[status_code]
+        self.message = message
+
+    @property
+    def error_response(self) -> ErrorResponse:
+        return ErrorResponse(
+            dict(error=dict(status=self.status_code, type=self.status_type, message=self.message)))
+
+
+def error_handlers(app: Flask) -> None:
+    def api_error(error: APIError) -> Tuple[ErrorResponse, int]:
+        error_response = jsonify(error.error_response)
+        return error_response, error.status_code
+
+    app.errorhandler(APIError)(api_error)
