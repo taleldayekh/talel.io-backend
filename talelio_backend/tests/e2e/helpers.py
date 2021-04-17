3a713ef60bb6e62c1ab788e49af2304f55cfa909
taleldayekh <mail@talel.se>
2021-04-17 10:14:56 +0200

    Extending e2e helpers and adding integration tests helpers

--- talelio_backend/tests/e2e/helpers.py
+++ talelio_backend/tests/e2e/helpers.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 import pytest
 from flask import Response
 
-from talelio_backend.tests.utils.constants import ACCOUNT_BASE_URL
+from talelio_backend.tests.utils.constants import ACCOUNTS_BASE_URL, USERS_BASE_URL
 
 
 @pytest.mark.usefixtures('api_server')
@@ -13,10 +13,17 @@ class RequestHelper:
     def register_account_request(self, registration_data: Dict[str, str]) -> Response:
         with patch('talelio_backend.app_account.domain.account_model.smtplib.SMTP_SSL'):
             return self.api.post(  # type: ignore
-                f'{ACCOUNT_BASE_URL}/register', json=registration_data)
+                f'{ACCOUNTS_BASE_URL}/register', json=registration_data)
 
     def verify_account_request(self, verification_token: str) -> Response:
-        return self.api.get(f'{ACCOUNT_BASE_URL}/verify/{verification_token}')  # type: ignore
+        return self.api.get(f'{ACCOUNTS_BASE_URL}/verify/{verification_token}')  # type: ignore
 
     def login_request(self, login_data: Dict[str, str]) -> Response:
-        return self.api.post(f'{ACCOUNT_BASE_URL}/login', json=login_data)  # type: ignore
+        return self.api.post(f'{ACCOUNTS_BASE_URL}/login', json=login_data)  # type: ignore
+
+    def get_user_projects_request(self, username: str) -> Response:
+        return self.api.get(f'{USERS_BASE_URL}/{username}/projects')  # type: ignore
+
+    def create_user_project_request(self, username: str, project: Dict[str, str]) -> Response:
+        return self.api.post(  # type: ignore
+            f'{USERS_BASE_URL}/{username}/projects', json=project)
--- talelio_backend/tests/integration/helpers.py
+++ talelio_backend/tests/integration/helpers.py
@@ -6,16 +6,18 @@ from talelio_backend.tests.utils.constants import EMAIL_TALEL, PASSWORD, USERNAM
 from talelio_backend.tests.utils.mocks import FakeUnitOfWork, generate_verification_token
 
 
-def account_registration_helper(uow: Union[FakeUnitOfWork, None] = None) -> FakeUnitOfWork:
+def account_registration_helper(uow: Union[FakeUnitOfWork, None] = None,
+                                email: str = EMAIL_TALEL,
+                                username: str = USERNAME_TALEL) -> FakeUnitOfWork:
     with patch('talelio_backend.app_account.domain.account_model.smtplib.SMTP_SSL'):
         if not uow:
             uow = FakeUnitOfWork()
 
-        register_account(uow, EMAIL_TALEL, PASSWORD, USERNAME_TALEL)  # type: ignore
+        register_account(uow, email, PASSWORD, username)  # type: ignore
 
         return uow
 
 
-def account_verification_helper(uow: FakeUnitOfWork) -> None:
-    verification_token = generate_verification_token({'email': EMAIL_TALEL})
+def account_verification_helper(uow: FakeUnitOfWork, email: str = EMAIL_TALEL) -> None:
+    verification_token = generate_verification_token({'email': email})
     verify_account(uow, verification_token)  # type: ignore
