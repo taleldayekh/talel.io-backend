38368ab12cd788dea022df3bfec6ac665eb286bf
taleldayekh <mail@talel.se>
2021-04-24 12:19:11 +0200

    Creating authorization decorator

--- talelio_backend/core/exceptions.py
+++ talelio_backend/core/exceptions.py
@@ -1,3 +1,7 @@
+class AuthorizationError(Exception):
+    pass
+
+
 class AccountError(Exception):
     pass
 
---
+++ talelio_backend/identity_and_access/authorization.py
@@ -0,0 +1,29 @@
+from functools import wraps
+from typing import Any, Union
+
+from talelio_backend.core.exceptions import AuthorizationError
+from talelio_backend.identity_and_access.authentication import verify_access_token
+
+
+def authorization_required(authorization_header: Union[str, None]) -> Any:
+    def decorator(func: Any) -> Any:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            if not authorization_header:
+                raise AuthorizationError('No authorization header provided')
+
+            token = authorization_header.split(' ')[1]
+
+            if not token:
+                raise AuthorizationError('No authorization token provided')
+
+            try:
+                verify_access_token(token)
+            except Exception as e:
+                raise AuthorizationError(e) from e
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
