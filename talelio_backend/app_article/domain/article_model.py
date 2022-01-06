639547f608320fcf62fbe22377b1387898349987
taleldayekh <dev@talel.io>
2022-01-06 10:26:06 +0000

    Adding initial article code

---
+++ talelio_backend/app_article/domain/article_model.py
@@ -0,0 +1,13 @@
+from markdown import markdown
+
+class Article:
+    def __init__(self, title: str, body: str) -> None:
+        self.title = title
+        self.body = body
+        self.html = ''
+
+    @property
+    def convert_body_to_html(self) -> None:
+        html = markdown(self.body)
+        self.html = html
+
