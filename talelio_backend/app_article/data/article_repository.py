b71e94f56eb63d31ca47799cdc8623b5721f863c
taleldayekh <dev@talel.io>
2022-01-08 11:05:24 +0100

    Creating article repository

---
+++ talelio_backend/app_article/data/article_repository.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+from talelio_backend.shared.data.repository import BaseRepository
+
+
+class ArticleRepository(BaseRepository):
+
+    def get(self, model: Any) -> Any:
+        return self.session.query(model).order_by(model.id.desc()).first()
