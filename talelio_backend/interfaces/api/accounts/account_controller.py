b5056f751340275aedae3a806ab4baa8a2e890fa
taleldayekh <dev@talel.io>
2022-01-15 12:58:28 +0100

    Adding api domain for cookies

--- talelio_backend/interfaces/api/accounts/account_controller.py
+++ talelio_backend/interfaces/api/accounts/account_controller.py
@@ -86,6 +86,7 @@ def login_endpoint() -> Tuple[Response, int]:
         res.set_cookie('refresh_token',
                        refresh_token,
                        httponly=True,
+                       domain='api.talel.io',
                        secure=not current_app.config['DEBUG'])
 
         return res, 200
