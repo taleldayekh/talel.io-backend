4a273bda33d16c9cde5e864bb759e83052b9f076
taleldayekh <dev@talel.io>
2022-02-10 19:21:47 +0100

    Adding fenced code blocks attr to markdown

--- talelio_backend/app_article/domain/article_model.py
+++ talelio_backend/app_article/domain/article_model.py
@@ -10,5 +10,5 @@ class Article:
 
     @property
     def convert_body_to_html(self) -> None:
-        html = markdown(self.body)
+        html = markdown(self.body, extensions=['fenced_code'])
         self.html = html
