ff7a6404dde9d0e79d74ded766d6868764c1880d
taleldayekh <dev@talel.io>
2022-09-07 18:32:52 +0200

    Revert after moving pagination query to articles repo

--- talelio_backend/app_user/data/user_repository.py
+++ talelio_backend/app_user/data/user_repository.py
@@ -1,9 +1,5 @@
 from typing import Any
 
-from sqlalchemy.orm import contains_eager
-from sqlalchemy.sql import text
-from sqlalchemy.sql.expression import func
-
 from talelio_backend.shared.data.repository import BaseRepository
 
 
@@ -15,16 +11,6 @@ class UserRepository(BaseRepository):
         if 'id' in kwargs:
             query = self.session.query(model).filter_by(id=kwargs.get('id')).first()
         if 'username' in kwargs:
-            username = kwargs.get('username')
-            limit = kwargs.get('limit')
-            offset = kwargs.get('offset')
-
-            query = self.session.query(
-                model,
-                func.count(model.articles).over().label('total_articles_count'))
-            query = query.filter_by(username=username)
-            query = query.join(model.articles).order_by(text('article.created_at desc'))
-            query = query.limit(limit).offset(offset)
-            query = query.options(contains_eager(model.articles)).populate_existing().all()
+            query = self.session.query(model).filter_by(username=kwargs.get('username')).first()
 
         return query
