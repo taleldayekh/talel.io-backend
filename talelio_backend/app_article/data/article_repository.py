b103a350082118a13c2175441415339ad7608be1
taleldayekh <dev@talel.io>
2022-11-20 00:36:25 +0100

    Query article together with prev and next article

--- talelio_backend/app_article/data/article_repository.py
+++ talelio_backend/app_article/data/article_repository.py
@@ -11,7 +11,30 @@ class ArticleRepository(BaseRepository):
 
     def get(self, model: Any, **kwargs: Any) -> Any:
         if 'slug' in kwargs:
-            query_res = self.session.query(model).filter_by(slug=kwargs.get('slug')).first()
+            # TODO: Improve querying to be more efficient instead of making multiple queries.
+            slug = kwargs.get('slug')
+
+            article_query_res = self.session.query(model).filter_by(slug=slug).first()
+
+            if article_query_res is not None:
+                next_article_query = self.session.query(model).with_entities(model.title, model.slug)
+                next_article_query = next_article_query.order_by(model.id.asc())
+                next_article_query_res = next_article_query.where(
+                    model.id > article_query_res.id).first()
+
+                prev_article_query = self.session.query(model).with_entities(model.title, model.slug)
+                prev_article_query = prev_article_query.order_by(model.id.desc())
+                prev_article_query_res = prev_article_query.where(
+                    model.id < article_query_res.id).first()
+            else:
+                next_article_query_res = None
+                prev_article_query_res = None
+
+            return {
+                'article': article_query_res,
+                'next_article_meta': next_article_query_res,
+                'prev_article_meta': prev_article_query_res
+            }
         if 'username' in kwargs:
             username = kwargs.get('username')
             limit = kwargs.get('limit')
