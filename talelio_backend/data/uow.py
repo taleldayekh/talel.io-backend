4a36f90e8fa884392bd66f846397921fc46871f9
taleldayekh <dev@talel.io>
2023-08-02 19:41:36 +0200

    Refactor to use db client rather than SQLAlchemy

--- talelio_backend/data/uow.py
+++ talelio_backend/data/uow.py
@@ -1,47 +1,79 @@
 from __future__ import annotations
 
 from types import TracebackType
-from typing import Optional, Type
-
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
+from typing import Any, Optional, Type
 
 from talelio_backend.app_account.data.account_repository import AccountRepository
-from talelio_backend.app_article.data.article_repository import ArticleRepository
-from talelio_backend.app_project.data.project_repository import ProjectRepository
-from talelio_backend.app_user.data.user_repository import UserRepository
-from talelio_backend.core.db import default_session
+from talelio_backend.libs.db_client import DbClient
 
 
 class UnitOfWork:
-    session: Session
+    session: Any
     account: AccountRepository
-    user: UserRepository
-    projects: ProjectRepository
-    articles: ArticleRepository
 
-    def __init__(self, session_factory: sessionmaker = default_session) -> None:
-        self.session_factory = session_factory
+    def __init__(self, db_client: DbClient = DbClient) -> None:
+        self.db_client = db_client()
 
     def __enter__(self: UnitOfWork) -> UnitOfWork:
-        self.session = self.session_factory()
+        self.session = self.db_client.get_connection
         self.account = AccountRepository(self.session)
-        self.user = UserRepository(self.session)
-        self.projects = ProjectRepository(self.session)
-        self.articles = ArticleRepository(self.session)
 
         return self
 
     def __exit__(self, exception_type: Optional[Type[BaseException]],
                  exception_value: Optional[BaseException],
                  traceback: Optional[TracebackType]) -> None:
-        if exception_type is not None:
-            self.session.rollback()
-
         self.session.close()
+        print('Db connection closed')
 
-    def flush(self) -> None:
-        self.session.flush()
 
-    def commit(self) -> None:
-        self.session.commit()
+# from __future__ import annotations
+
+# from types import TracebackType
+# from typing import Optional, Type
+
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.orm.session import Session
+
+# from talelio_backend.app_account.data.account_repository import AccountRepository
+# from talelio_backend.app_article.data.article_repository import ArticleRepository
+# from talelio_backend.app_project.data.project_repository import ProjectRepository
+# from talelio_backend.app_user.data.user_repository import UserRepository
+# from talelio_backend.core.db import default_session
+
+# from talelio_backend.libs.db_client import DbClient
+
+# class UnitOfWork:
+#     db: DbClient
+#     account: AccountRepository
+#     # user: UserRepository
+#     # projects: ProjectRepository
+#     # articles: ArticleRepository
+
+#     def __init__(self, db_client: DbClient = DbClient) -> None:
+#         self.db_client = db_client
+
+#     def __enter__(self: UnitOfWork) -> UnitOfWork:
+#         self.db = self.db_client()
+#         self.account = AccountRepository(self.db)
+#         # self.user = UserRepository(self.db)
+#         # self.projects = ProjectRepository(self.db)
+#         # self.articles = ArticleRepository(self.db)
+
+#         return self
+
+#     def __exit__(self, exception_type: Optional[Type[BaseException]],
+#                  exception_value: Optional[BaseException],
+#                  traceback: Optional[TracebackType]) -> None:
+#         ! Do I need to take this into consideration
+#         if exception_type is not None:
+#             self.db.rollback()
+
+#         self.db.close()
+
+#     # TODO: Delete these methods
+#     def flush(self) -> None:
+#         self.db.flush()
+
+#     def commit(self) -> None:
+#         self.db.commit()
--- talelio_backend/shared/data/repository.py
+++ talelio_backend/shared/data/repository.py
@@ -1,15 +1,7 @@
 from typing import Any
 
-from sqlalchemy.orm.session import Session
-
 
 class BaseRepository:
-
-    def __init__(self, session: Session) -> None:
+    # TODO: Find correct type for session
+    def __init__(self, session: Any) -> None:
         self.session = session
-
-    def add(self, model: Any) -> Any:
-        self.session.add(model)
-        self.session.flush()
-
-        return model
